3ab4e5ae1f6382fe03ca0ae144bbd162553a13c9
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-20 16:42:31 +0300

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -11,7 +12,7 @@ morgan.token("body", (req, res) => {
 // Middlewares
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
-``;
+app.use(cors());
 
 let entries = [
   {
