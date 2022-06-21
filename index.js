820faea391dbd59c61aa5907b3bd24dfa83f6757
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-21 15:37:15 +0300

    create person model, get from mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -15,31 +17,11 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static("build"));
 
-let entries = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(entries);
+  Person.find({}).then((persons) => {
+    console.log(persons);
+    res.json(persons);
+  });
 });
 
 // PERSONS
---
+++ models/person.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const url = process.env.MONGO_URI;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => console.log(`Error: ${err.message}`));
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  number: { type: Number, required: true },
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
