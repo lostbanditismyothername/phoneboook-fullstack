bf2d22afff5c9dbae413206dabd6075bcf24e2e7
Ozgur Cetin <oz.ozgurcetin@gmail.com>
2022-06-22 17:57:30 +0300

    add error handler middleware

--- index.js
+++ index.js
@@ -11,31 +11,47 @@ morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
 
+// Error handler
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 // Middlewares
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 app.use(cors());
 app.use(express.static("build"));
+app.use(errorHandler);
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 // PERSONS
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = Number(req.params.id);
-  Person.findById(id).then((person) => {
-    if (person) {
-      res.json(person);
-    } else {
-      res.status(404).end();
-    }
-  });
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).json({ error: "No such person with the ID" });
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons/", (req, res) => {
+app.post("/api/persons/", (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -49,22 +65,32 @@ app.post("/api/persons/", (req, res) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  entries = entries.filter((entry) => entry.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then((returnedPerson) => {
+      if (!returnedPerson) {
+        res.status(404).json({ error: "no such person found with the ID" });
+      } else {
+        res.status(204).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 // INFO
-app.get("/api/info", (req, res) => {
-  res.send(`<p>Phonebook has info about ${entries.length} people</p>
-  <p>${new Date()}</p>`);
-});
+// app.get("/api/info", (req, res) => {
+//   res.send(`<p>Phonebook has info about ${entries.length} people</p>
+//   <p>${new Date()}</p>`);
+// });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
